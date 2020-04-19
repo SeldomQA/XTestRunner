7a6141eef74459f943e0e5c16f72aec7effd2f16
fnngj <fnngj@126.com>
2020-04-19 20:50:56 +0800

    css引用

--- HTMLTestRunner.py
+++ HTMLTestRunner.py
@@ -184,10 +184,7 @@ class Template_mixin(object):
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <script src="http://apps.bdimg.com/libs/Chart.js/0.2.0/Chart.min.js"></script>
-    <!-- <link href="https://cdn.bootcss.com/echarts/3.8.5/echarts.common.min.js" rel="stylesheet">   -->
-    <!-- <link rel="stylesheet" href="https://cdn.bootcss.com/twitter-bootstrap/4.3.1/css/bootstrap.min.css"> -->
-    <link rel="stylesheet" href="./classic.css">
-
+    <link rel="stylesheet" href="http://img.itest.info/classic.css">
 
     %(stylesheet)s
 </head>
