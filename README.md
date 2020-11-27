d11ad1d857716151c84be197027b2357ca8174d9
defnngj <defnngj@gmail.com>
2020-11-27 18:23:01 +0800

    add install

--- README.md
+++ README.md
@@ -7,10 +7,13 @@ HTMLTestRunner:http://tungwaiyip.info/software/HTMLTestRunner.html
 介绍：
   HTMLTestRunner是针对Python2.x的unittest单元测试框架用于生成HTML测试报告的扩展。但是，它并不支持Python3.x,所以，我做了一点小小的修改，使其可以在Python3下使用。
 
-依赖：
+
+## install
 
 ```shell
-pip install jinja2==2.11.2
+git clone https://github.com/SeldomQA/HTMLTestRunner
+cd HTMLTestRunner/
+python setup.py install
 ```
 
 ## demo
---
+++ setup.py
@@ -0,0 +1,38 @@
+# coding=utf-8
+import re
+import ast
+from setuptools import setup, find_packages
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+
+with open('TestRunner/__init__.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+        f.read().decode('utf-8')).group(1)))
+
+setup(
+    name='TestRunner',
+    version=version,
+    url='https://github.com/seldomQA/HTMLTestRunner/',
+    license='BSD',
+    author='bugmaster',
+    author_email='fnngj@126.com',
+    description='Unittest-based HTML test report.',
+    long_description="Unittest-based HTML test report.",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'jinja2>=2.11.2',
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        "Topic :: Software Development :: Testing",
+    ],
+)
