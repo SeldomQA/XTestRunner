717e640319b4f67ec76e63a5e65ae31613830607
defnngj <defnngj@gmail.com>
2022-03-05 20:09:54 +0800

    ✨ support label

--- XTestRunner/config.py
+++ XTestRunner/config.py
@@ -8,3 +8,21 @@ class RunResult:
     errors = 0
     skipped = 0
 
+
+def label(*labels):
+    """
+    Test case classification label
+
+    Usage:
+        @label('quick')
+        class MyTest(unittest.TestCase):
+            def test_foo(self):
+                pass
+    """
+
+    def inner(cls):
+        # append labels to class
+        cls._labels = set(labels) | getattr(cls, '_labels', set())
+        return cls
+
+    return inner
