a76f241ec1d60c727e1e5422ef06dd79b0a1c465
defnngj <defnngj@gmail.com>
2022-03-07 00:49:21 +0800

    ✔️update test selenium demo

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -2,6 +2,7 @@ import unittest
 from time import sleep
 from XTestRunner import HTMLTestRunner
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 """
 1.安装selenium
@@ -16,35 +17,46 @@ class YouTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.driver = webdriver.Chrome()
-        cls.base_url = "https://www.baidu.com"
+        cls.base_url = "https://cn.bing.com/"
 
     @classmethod
     def tearDownClass(cls) -> None:
         cls.driver.quit()
 
     def test_success(self):
-        """测试百度搜索：HTMLTestRunner """
+        """测试bing搜索：XTestRunner """
         self.driver.get(self.base_url)
-        self.driver.find_element_by_id("kw").send_keys("XTestRunner")
-        self.driver.find_element_by_id("su").click()
+        sleep(2)
+        search = self.driver.find_element(By.ID, "sb_form_q")
+        search.send_keys("XTestRunner")
+        search.submit()
         sleep(2)
 
     def test_error(self):
-        """测试百度搜索，定位失败 """
+        """测试bing搜索，定位失败 """
         self.driver.get(self.base_url)
-        self.driver.find_element_by_id("kw").send_keys("python")
-        self.driver.find_element_by_id("su22").click()
+        sleep(2)
+        self.driver.find_element(By.ID, "sb_form_qxxx").send_keys("python")
         sleep(2)
 
     def test_fail(self):
-        """测试百度搜索，断言失败 """
+        """测试bing搜索，断言失败 """
         self.driver.get(self.base_url)
-        self.driver.find_element_by_id("kw").send_keys("unittest")
-        self.images.append(self.driver.get_screenshot_as_base64())
-        self.driver.find_element_by_id("su").click()
-        sleep(2)
-        self.images.append(self.driver.get_screenshot_as_base64())
+        self.driver.find_element(By.ID, "sb_form_q").send_keys("unittest")
         self.assertEqual(self.driver.title, "unittest")
+
+    def test_screenshots(self):
+        """测试截图"""
+        self.driver.get(self.base_url)
+        sleep(2)
+        # 元素截图
+        elem = self.driver.find_element(By.ID, "sb_form_q")
+        self.images.append(elem.screenshot_as_base64)
+        # 竖屏截图
+        self.images.append(self.driver.get_screenshot_as_base64())
+        # 最大化截图
+        self.driver.maximize_window()
+        self.images.append(self.driver.get_screenshot_as_base64())
 
 
 if __name__ == '__main__':
@@ -52,6 +64,7 @@ if __name__ == '__main__':
     suit.addTest(YouTest("test_success"))
     suit.addTest(YouTest("test_error"))
     suit.addTest(YouTest("test_fail"))
+    suit.addTest(YouTest("test_screenshots"))
 
     report = "./reports/selenium_result.html"
     with(open(report, 'wb')) as fp:
