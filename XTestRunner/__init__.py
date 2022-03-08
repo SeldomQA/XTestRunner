d0c03ef79fe34936927cee67c34ac851e44b1393
defnngj <defnngj@gmail.com>
2022-03-08 23:19:33 +0800

    ✨️XMLTestRunner

--- XTestRunner/__init__.py
+++ XTestRunner/__init__.py
@@ -31,6 +31,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # URL: https://github.com/SeldomQA/XTestRunner
 
+from .xmlrunner.runner import XMLTestRunner
 from .HTMLTestRunner import HTMLTestRunner
 from .email import SMTP
 from .config import label
