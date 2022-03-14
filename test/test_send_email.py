7884877fdaa8e719d4857e7aacc63a74d02d3e56
defnngj <defnngj@gmail.com>
2022-03-15 00:02:33 +0800

    ✔️update send email test

--- test/test_send_email.py
+++ test/test_send_email.py
@@ -2,6 +2,12 @@ import unittest
 from XTestRunner import HTMLTestRunner
 from XTestRunner import SMTP
 
+"""
+说明：
+1.使用126邮箱发送时password应为授权码而非用户密码，须在邮箱客户端设置开启授权码
+2.使用gmail邮箱发送时password为用户密码，须在gmail客户端开启安全性较低的应用的访问权限
+"""
+
 
 class TestDemo(unittest.TestCase):
     """测试用例说明"""
@@ -22,15 +28,33 @@ class TestDemo(unittest.TestCase):
 
 
 if __name__ == '__main__':
-    report = "./reports/send_email_result.html"
+    suit = unittest.TestSuite()
+    suit.addTests([
+        TestDemo("test_success"),
+        TestDemo("test_skip"),
+        TestDemo("test_fail"),
+        TestDemo("test_error")
+    ])
+
+    report = "./test/reports/email_result.html"
     with(open(report, 'wb')) as fp:
-        unittest.main(testRunner=HTMLTestRunner(
+        runner = HTMLTestRunner(
             stream=fp,
             title='测试发送邮件',
-            description=['类型：测试发送邮件', '执行人：虫师']
-        ))
-    # 发邮件功能
-    # 使用126邮箱发送时password应为授权码而非用户密码，须在邮箱客户端设置开启授权码
-    # 使用gmail邮箱发送时password为用户密码，须在gmail客户端开启安全性较低的应用的访问权限
+            tester='虫师',
+            description=['类型：测试发送邮件'],
+            language="zh-CN"
+        )
+        runner.run(suit)
+        # 发送邮件方式 1：send_email()方法
+        runner.send_email(
+            user="sender@qq.com",
+            password="xxx",
+            host="smtp.qq.com",
+            to="recipient@126.com",
+            subject="测试邮件",
+            attachments=report
+        )
+    # 发送方式 2：SMTP类
     smtp = SMTP(user="sender@qq.com", password="xxx", host="smtp.qq.com")
-    smtp.sender(to="fnngj@126.com", subject="XTestRunner测试邮件", attachments=report)
+    smtp.sender(to="recipient@126.com", subject="XTestRunner测试邮件", attachments=report)
